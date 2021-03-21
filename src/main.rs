b7a6efdeddc6edf97c25d978752c5bb148cc8f59
Adnan <adnan_ghafoor94@hotmail.com>
2021-03-21 11:03:56 +0000

    Learning rust
    Learning how to use Rust with a guessing game

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Guess the number game");
+    println!("Enter your guess");
+    // mut makes the variable mutable
+    let mut guess = String::new();
+    // String::new() creates an empty instance of a string
+    std::io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    /*
+       std::io can be above the function to import the namespace then can use io::stdin
+       It is like C++ std etc. string argument needs to be mutable so answers can change
+       & is a reference to the variable without having to store new variables in memory to access it multiple times
+       Refernces are immutable by default so need to add mut to make it mutable
+       read_line returns a value in this case std::io::Result
+    */
+    println!("You guessed: {}", guess);
+}
